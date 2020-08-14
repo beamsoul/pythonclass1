ca7ef508b723611215298ba6e4aab385fa3c772b
Janna <janna@Jannas-MacBook-Air.local>
2020-08-14 14:30:18 -0500

    adding permetr

---
+++ area_perimetr.py
@@ -0,0 +1,7 @@
+x=float(input('Please define a length of a square: '))
+y=float(input("Please define a width of a square "))
+s=x*y
+p=(x+y)*2
+print("The area of a square equal to", s)
+print("The perimert of a square equal to", p )
+
